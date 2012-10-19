17346fce0f4bb58d8255f19d5c4854a8141fce0c
Bob Fang <dorafmon@gmail.com>
2012-10-19 11:32:12 +0100

    Insertion_sort

---
+++ Sorter/Sorter.py
@@ -0,0 +1,18 @@
+class Sorter:
+    
+    def insertionSort(self,A):
+        i=0
+        while i<len(A):
+            key=A[i]
+            j=i-1
+            while j>0 and A[j]>key:
+                A[j+1]=A[j]
+                j=j-1
+            A[j+1]=key
+            i=i+1
+        return A
+
+if __name__=="__main__":
+    s = Sorter()
+    B=s.insertionSort([5,3,1,6,4,2,9,7])
+    print B
