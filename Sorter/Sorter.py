59d265e2f0dc7671faaea2fe6e211398e6deaa48
Bob Fang <dorafmon@gmail.com>
2012-10-21 23:18:18 +0100

    Insertion-Sort Corrected

--- Sorter/Sorter.py
+++ Sorter/Sorter.py
@@ -5,7 +5,7 @@ class Sorter:
         while i<len(A):
             key=A[i]
             j=i-1
-            while j>0 and A[j]>key:
+            while j>=0 and A[j]>key:
                 A[j+1]=A[j]
                 j=j-1
             A[j+1]=key
