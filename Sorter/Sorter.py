41bc7579be2347445bce5cc34c734443e6486a55
Bob Fang <dorafmon@gmail.com>
2012-10-28 20:08:31 +0000

    Merge Sort

--- Sorter/Sorter.py
+++ Sorter/Sorter.py
@@ -1,4 +1,13 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+import random
+import time
+
 class Sorter:
+    def largeRandomArray(self,number):
+        A=[]
+        for i in range(0,number):
+            A.append(int(random.random()*number))
+        return A
     
     def insertionSort(self,A):
         i=0
@@ -11,8 +20,41 @@ class Sorter:
             A[j+1]=key
             i=i+1
         return A
+ 
+    def merge(self,A,B):
+        index_1=len(A)
+        index_2=len(B)
+        pointer_1=0
+        pointer_2=0
+        R=[]
+        while pointer_1<index_1 and pointer_2<index_2:
+            if A[pointer_1] <= B[pointer_2]:
+                R.append(A[pointer_1])
+                pointer_1+=1
+            else:
+                R.append(B[pointer_2])
+                pointer_2+=1
+        if pointer_1 == index_1:
+            R.extend(B[pointer_2:len(B)])
+        else:
+            R.extend(A[pointer_1:len(A)])
+        return R
+
+    def mergeSort(self,A):
+        if len(A) != 1 :
+            l=self.mergeSort(A[0:len(A)/2])
+            r=self.mergeSort(A[len(A)/2:len(A)])
+            R=self.merge(l,r)
+            return R
+        else:
+            return A
 
 if __name__=="__main__":
-    s = Sorter()
-    B=s.insertionSort([5,3,1,6,4,2,9,7])
-    print B
+    import profile
+    s=Sorter()
+    l=s.largeRandomArray(10000)
+    print ("================PROFILE=================")
+    print ("Insertion Sort:")
+    profile.run("s.insertionSort(l)")
+    print ("Merge Sort:")
+    profile.run("s.mergeSort(l)")
